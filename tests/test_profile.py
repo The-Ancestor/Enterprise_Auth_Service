c19e4b88e576f6ff59954769009eaad6d4dc09a7
me <me@kali.KALI.COM>
2026-08-27 00:48:48 +0200

    test: update profile test variables

--- tests/test_profile.py
+++ tests/test_profile.py
@@ -1,13 +1,16 @@
 def test_profile_success(client):
-    response = client.post("/auth/register", json={"email" :  "aws@gmail.com", "password" : "12345678"})
+    email = "test@gmail.com"
+    password = "Pytest"
+    response = client.post("/auth/register", json={"email" : email, "password" : password})
     assert response.status_code == 200
-    login_res = client.post("/auth/login", json={"email": "aws@gmail.com", "password": "12345678"}, headers={"User-Agent": "PytestClient/1.0"})
+    login_res = client.post("/auth/login", json={"email": email, "password": password}, headers={"User-Agent": "PytestClient/1.0"})
+    assert response.status_code == 200
     
     token = login_res.json()["token"]
     response = client.get("/auth/profile", headers={"Authorization": f"Bearer {token}"})
     
     assert response.status_code == 200
     data = response.json()
-    assert data["email"] == "aws@gmail.com"
+    assert data["email"] == email
     assert len(data["sessions"]) == 1
     assert data["sessions"][0]["user_agent"] == "PytestClient/1.0"
